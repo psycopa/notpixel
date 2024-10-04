ad36ea777faed35234dce6e764bc2132c6e11509
psycopa <93734425+psycopa@users.noreply.github.com>
2024-10-05 00:17:21 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,6 @@
-# put yours
-API_ID = 23477584
-API_HASH = "4ee989d60366feebdff6b26b7efec732"
-
 # =============[upgrades]================
 # default is 5 level you can change it your self
 PAINT_REWARD_MAX = 7 # max is 7
 ENERGY_LIMIT_MAX = 7 # max is 6
 RE_CHARGE_SPEED_MAX = 11 # max is 11
 
-# ================[proxy]================
-USE_PROXY = False # or put True if you want use it
-PROXIES = {
-    "http":"socks5://127.0.0.1",
-    "https":"socks5://127.0.0.1", # if you using socks4 or http only replace it with "socks5"
-}
