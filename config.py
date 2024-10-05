66c3400b3adcbe1cf90d1f02c58c36eb8e4bccff
psycopa <93734425+psycopa@users.noreply.github.com>
2024-10-05 19:23:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 API_ID = 
 API_HASH = "idhash"
-# =============[upgrades]================
-# default is 5 level you can change it your self
-PAINT_REWARD_MAX = 7 # max is 7
-ENERGY_LIMIT_MAX = 7 # max is 6
-RE_CHARGE_SPEED_MAX = 11 # max is 11
+# list upgrade ubah level sesual keinginan
+PAINT_REWARD_MAX = 7 # max 7
+ENERGY_LIMIT_MAX = 7 # max 6
+RE_CHARGE_SPEED_MAX = 11 # max 11
 
